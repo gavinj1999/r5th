36fa2d34f5fbde524c6fd90923986a5d5f0b7a23
gavinj1999 <gavinj2006@googlemail.com>
2014-08-31 20:02:48 +0100

    newfilesadded
    fdsaf

---
+++ index.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html>
+<head>
+	
+	<meta charset="utf-8">
+<title>Rising 5th Web Design|Web Design Cheshire</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>Hydrogen - HTML5 One Page Template</title>
+
+<!-- Fonts -->
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Raleway:300,400,600,700,300italic,400italic,600italic,700italic|Droid+Serif:400,400italic">
+<link rel="stylesheet" href="assets/fonts/novecento/stylesheet.css">
+<link rel="stylesheet" href="assets/icons/glyphicons/style.min.css">
+<link rel="stylesheet" href="assets/icons/font-awesome/font-awesome.min.css">
+
+<!-- Styles -->
+<link rel="stylesheet" href="assets/css/bootstrap.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/theme-light.css">
+<link rel="stylesheet" href="assets/css/theme-dark.css">
+
+<!-- Plugins -->
+<link rel="stylesheet" href="assets/plugins/royalslider/royalslider.min.css">
+<link rel="stylesheet" href="assets/plugins/owlcarousel/owl.carousel.min.css">
+<link rel="stylesheet" href="assets/plugins/mfp/jquery.mfp.css">
+
+
+</head>
+
+<body>
+	<section id="home" class="site-splash fullscreen">
+
+	<div class="splash-inner">
+
+		<div class="splash-slider">
+
+			<div class="splash-content"><div class="container">
+
+					<div class="row">
+
+						<div class="col-md-12">
+
+							<div class="splash-text">
+								<h4 class="splash-intro"><a href="#">Hello there!</a></h4>
+								<h1 class="splash-headline">We are Rising 5th</h1>
+								<div class="splash-description">
+									<p>A Cheshire based Web Development Agency</p>
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+
+			</div></div>
+
+			<div class="splash-content"><div class="container">
+
+					<div class="row">
+
+						<div class="col-md-12">
+
+							<div class="splash-text">
+								<h4 class="splash-intro"><a href="#">We're Different</a></h4>
+								<h1 class="splash-headline">We Connect your business with the Web</h1>
+								<div class="splash-description">
+									<p>linking the physical to the technical</p>
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+
+			</div></div>
+
+			<div class="splash-content"><div class="container">
+
+					<div class="row">
+
+						<div class="col-md-12">
+
+							<div class="splash-text">
+								<h4 class="splash-intro"><a href="#">Let's Talk Today</a></h4>
+								<h1 class="splash-headline">Have a project? Let's talk.</h1>
+								<div class="splash-description">
+									<p>Every client working with us are always satisfied, we can prove it!</p>
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+
+			</div></div>
+
+			<div class="cycle-next"></div>
+			<div class="cycle-prev"></div>
+
+		</div>
+
+		<div class="splash-media" 
+			data-background="demo/img/splash-bg.jpg" data-background-xs="demo/img/splash-bg-xs.jpg" data-background-md="demo/img/splash-bg-md.jpg" data-background-lg="demo/img/splash-bg-lg.jpg">
+			<div class="overlay"></div>
+		</div>
+
+		<div class="splash-feedback">
+			<span class="mouse">
+				<span class="fa fa-angle-down"></span>
+			</span>
+			<span class="caption">Scroll</span>
+		</div>
+
+	</div>
+
+</section>
+<script type="text/javascript" src="assets/js/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="assets/js/hydrogen.setup.min.js"></script>
+<script type="text/javascript" src="assets/js/hydrogen.scripts.min.js"></script>
+<script type="text/javascript" src="assets/js/hydrogen.plugins.js"></script>
+
+<script type="text/javascript" src="assets/plugins/royalslider/jquery.royalslider.min.js"></script>
+<script type="text/javascript" src="assets/plugins/mfp/jquery.mfp-0.9.9.min.js"></script>
+<script type="text/javascript" src="assets/plugins/mediaelement/mediaelement-and-player.min.js"></script>
+<script type="text/javascript" src="assets/plugins/gmap/gmap3.min.js"></script>
+<script type="text/javascript" src="assets/plugins/owlcarousel/owl.carousel.min.js"></script>
+<script type="text/javascript" src="assets/plugins/isotope/jquery.isotope.min.js"></script>
+<script type="text/javascript" src="assets/plugins/form/jquery.form.min.js"></script>
+<script type="text/javascript" src="assets/plugins/form/jquery.validate.min.js"></script>
+</body>
+</html>
